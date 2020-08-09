6e4f4070b92aa8be1cffe632de29f199c46b56e5
Joel <joel.roca.martinez@hotmail.com>
2020-08-09 16:13:05 +0200

    latest changes in

--- routes/private.routes.js
+++ routes/private.routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const UserModel = require('../models/User.model.js')
 const DreamModel = require('../models/Dream.model.js')
-
 //Configuration for Cloudinary and Recording
 const cloudinary = require("cloudinary").v2
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
@@ -13,7 +12,6 @@ cloudinary.config({
     api_key: process.env.API_KEY, 
     api_secret: process.env.API_SECRET 
     });
-    
     const storage = new CloudinaryStorage({
       cloudinary,
       folder: 'Records', // The name of the folder in cloudinary
@@ -23,16 +21,12 @@ cloudinary.config({
         cb(null, res.originalname); // The file on cloudinary would have the same name as the original file name
       }
     });
-    
     const uploader = multer({ storage });
 // -----------------------------------------
-
-
 // Create a dream page
 router.get('/record', (req, res) => {
   res.render('users/record.hbs', {loggedInUser: req.session.loggedInUser})
 });
-
 // Post route for the text part of the form
 router.post('/record', (req, res) => {
   console.log("In the post record route")
@@ -50,7 +44,6 @@ DreamModel.create({title, categories, description, date, owner})
   res.render("users/record.hbs", {failed : true})
 })
 });
-
 // Post for the recording part (store it in cloudinary and in the db)
 router.post('/upload', uploader.single('audio_data'), (req, res, next) => {
 console.log('file is: ', req.file)
@@ -61,33 +54,28 @@ if (!req.file) {
 let audioUrl = req.file.path
 DreamModel.findOneAndUpdate({title: req.session.title}, {$set: {audioUrl}})
   .then(() => {
-    res.redirect('/dreams')})
+    console.log("before redirect")
+    res.redirect('/dreams')
+  })
   .catch((err) => {
       console.log(err)
       res.render("users/record.hbs", {failed : true})
    })
 })
-
-
-
 // Private list of dreams
 router.get('/dreams', (req, res) => {
   DreamModel.find({owner: req.session.loggedInUser._id})
       .then((result) => {
           res.render('users/dreams.hbs', {result})
-      });
+      })
+      .catch((err) => console.log(err));
 });
-
-
 router.post('/dreams', (req, res) => {
   DreamModel.find({owner: req.session.loggedInUser._id})
       .then((result) => {
           res.render('users/dreams.hbs', {result})
       });
 });
-
-
-
 // Edit dreams
 router.get('/dreams/:id/edit', (req, res, next) => {
 DreamModel.findById(req.params.id)
@@ -97,8 +85,6 @@ DreamModel.findById(req.params.id)
      res.render("users/edit.hbs", {result, publicStatus})
 })
 });
-
-
 router.post('/dreams/:id/edit', (req, res, next) => {
   console.log(req.body)
 let {title, categories, description, date, status} = req.body
@@ -112,30 +98,24 @@ DreamModel.findByIdAndUpdate(dreamId, {$set: {title, categories, description, da
       res.render("users/record.hbs", {failed : true})
    })
 });
-
-// Dreams delete 
-router.post('/dreams/:id/delete', (req, res, next) => {
+// Delete Dream
+router.post('/dreams/:id/delete', (req, res) => {
   DreamModel.findByIdAndDelete(req.params.id)
-    .then(() => {
-      res.redirect("/dreams")
-  })
-
+    .then(() => res.redirect('/dreams'))
+    .catch((err) => {
+      console.log(err)
+   })
 })
-
-
-
 // Dream details
 router.get('/dreams/:id/details', (req, res, next) => {
   DreamModel.findById(req.params.id)
   .then((result) => {
     let privateStatus;
-    result.status == "private"? privateStatus = true : privateStatus = false
+    result.status == "private"? privateStatus = true : privateStatus = false;
      res.render("users/dream-details.hbs", {result, privateStatus})
   })
+  .catch((err) => console.log(err))
 });
-  
-
-
 // Public list of dreams
 router.get('/dreamFlow', (req, res) => {
   DreamModel.find({status: "public"})
@@ -143,9 +123,4 @@ router.get('/dreamFlow', (req, res) => {
           res.render('users/dream-flow.hbs', {result})
       });
 });
-
-
-
-
-
 module.exports = router;
