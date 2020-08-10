d3286b63477d8c30d72dfbe024f6b98989dd7e16
Elise Djn <elisedjn@protonmail.com>
2020-08-10 12:37:24 +0200

    show audio tag after recording

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,150 +1,188 @@
 URL = window.URL || window.webkitURL;
 var gumStream;
-//stream from getUserMedia() 
+//stream from getUserMedia()
 var rec;
-//Recorder.js object 
+//Recorder.js object
 var input;
-//MediaStreamAudioSourceNode we'll be recording 
-// shim for AudioContext when it's not avb. 
+//MediaStreamAudioSourceNode we'll be recording
+// shim for AudioContext when it's not avb.
 var AudioContext;
 var audioContext;
 let upload;
-//new audio context to help us record 
+//new audio context to help us record
 var recordButton = document.getElementById("recordButton");
 var stopButton = document.getElementById("stopButton");
-//add events to those 3 buttons 
+//add events to those 3 buttons
 recordButton.addEventListener("click", startRecording);
 stopButton.addEventListener("click", stopRecording);
 
+function startRecording() {
+  console.log("recordButton clicked");
+  var constraints = {
+    audio: true,
+    video: false,
+  };
+  var AudioContext = window.AudioContext || window.webkitAudioContext;
+  var audioContext = new AudioContext();
 
-function startRecording() { console.log("recordButton clicked"); 
-var constraints = {
-  audio: true,
-  video: false
-} 
-var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioContext = new AudioContext;
+  /* Disable the record button until we get a success or fail from getUserMedia() */
+  recordButton.disabled = true;
+  stopButton.disabled = false;
 
-/* Disable the record button until we get a success or fail from getUserMedia() */
-recordButton.disabled = true;
-stopButton.disabled = false;
-
-// We're using the standard promise based getUserMedia()
-navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-  console.log("getUserMedia() success, stream created, initializing Recorder.js ..."); 
-  /* assign to gumStream for later use */
-  gumStream = stream;
-  /* use the stream */
-  input = audioContext.createMediaStreamSource(stream);
-  /* Create the Recorder object and configure to record mono sound (1 channel) Recording 2 channels will double the file size */
-  rec = new Recorder(input, {
-      numChannels: 1
-  }) 
-  //start the recording process 
-  rec.record()
-  console.log("Recording started");
-}).catch(function(err) {
-  //enable the record button if getUserMedia() fails 
-  recordButton.disabled = false;
-  stopButton.disabled = true;
-});
+  // We're using the standard promise based getUserMedia()
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function (stream) {
+      console.log(
+        "getUserMedia() success, stream created, initializing Recorder.js ..."
+      );
+      /* assign to gumStream for later use */
+      gumStream = stream;
+      /* use the stream */
+      input = audioContext.createMediaStreamSource(stream);
+      /* Create the Recorder object and configure to record mono sound (1 channel) Recording 2 channels will double the file size */
+      rec = new Recorder(input, {
+        numChannels: 1,
+      });
+      //start the recording process
+      rec.record();
+      console.log("Recording started");
+    })
+    .catch(function (err) {
+      //enable the record button if getUserMedia() fails
+      recordButton.disabled = false;
+      stopButton.disabled = true;
+    });
 }
 
 function stopRecording() {
   console.log("stopButton clicked");
-  //disable the stop button, enable the record too allow for new recordings 
+  //disable the stop button, enable the record too allow for new recordings
   stopButton.disabled = true;
   recordButton.disabled = false;
-  //tell the recorder to stop the recording 
-  rec.stop(); //stop microphone access 
+  //tell the recorder to stop the recording
+  rec.stop(); //stop microphone access
   gumStream.getAudioTracks()[0].stop();
-  
+  rec.exportWAV(createDownloadLink);
 }
 
 function createDownloadLink(blob) {
-	var url = URL.createObjectURL(blob);
-	var au = document.createElement('audio');
-	var li = document.createElement('li');
+  var url = URL.createObjectURL(blob);
+  var au = document.createElement("audio");
+  var li = document.createElement("li");
 
-	//name of .wav file to use during upload and download (without extendion)
+  //name of .wav file to use during upload and download (without extendion)
   var filename = new Date().toISOString();
 
-	//add controls to the <audio> element
-	au.controls = true;
-	au.src = url;
+  //add controls to the <audio> element
+  au.controls = true;
+  au.src = url;
 
+  //add the new audio element to li
+  li.appendChild(au);
 
-	//add the new audio element to li
-	li.appendChild(au);
-	
-	//add the filename to the li
-	li.appendChild(document.createTextNode(filename))
-	
-    // XMLHTTP Request
-		  var xhr=new XMLHttpRequest();
-		  xhr.onload=function(e) {
-		      if(this.readyState === 4) {
-              window.location.href="/dreams"
-		      }
-      };
-      var fd=new FormData();
-      fd.append("audio_data",blob, filename);
-      xhr.open("POST","/upload",true);
-      
+  //add the filename to the li
+  li.appendChild(document.createTextNode(filename));
 
-      let myBody = {
-        title: document.querySelector("#nameYourDream").value,
-        description: document.querySelector("#description").value,
-        date: document.querySelector("#date").value,
-        categories: ["Action"]
-      }
+  // // XMLHTTP Request
+  //   var xhr=new XMLHttpRequest();
+  //   xhr.onload=function(e) {
+  //       if(this.readyState === 4) {
+  //           window.location.href="/dreams"
+  //       }
+  //   };
+  //   var fd=new FormData();
+  //   fd.append("audio_data",blob, filename);
+  //   xhr.open("POST","/upload",true);
 
-      console.log(myBody)
+  //   let myBody = {
+  //     title: document.querySelector("#nameYourDream").value,
+  //     description: document.querySelector("#description").value,
+  //     date: document.querySelector("#date").value,
+  //     categories: ["Action"]
+  //   }
 
-      fetch('/record', {
-        method: "POST",
-        body: JSON.stringify(myBody),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8'
-        }
-      })
-      .then(() => {
-        console.log("fetch done")
-        xhr.send(fd);
-      })
-      .catch((err) => console.log(err))
-      
+  //   console.log(myBody)
 
-	//add the li element to the ul
-	recordingsList.appendChild(li);
+  //   fetch('/record', {
+  //     method: "POST",
+  //     body: JSON.stringify(myBody),
+  //     headers: {
+  //       'Content-type': 'application/json; charset=UTF-8'
+  //     }
+  //   })
+  //   .then(() => {
+  //     console.log("fetch done")
+  //     xhr.send(fd);
+  //   })
+  //   .catch((err) => console.log(err))
+
+  //add the li element to the ul
+  recordingsList.appendChild(li);
 }
 
+function sendData(blob) {
+  var filename = new Date().toISOString();
+  // XMLHTTP Request
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function (e) {
+    if (this.readyState === 4) {
+      window.location.href = "/dreams";
+    }
+  };
+  var fd = new FormData();
+  fd.append("audio_data", blob, filename);
+  xhr.open("POST", "/upload", true);
+
+  let myBody = {
+    title: document.querySelector("#nameYourDream").value,
+    description: document.querySelector("#description").value,
+    date: document.querySelector("#date").value,
+    categories: ["Action"],
+  };
+
+  console.log(myBody);
+
+  fetch("/record", {
+    method: "POST",
+    body: JSON.stringify(myBody),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then(() => {
+      console.log("fetch done");
+      xhr.send(fd);
+    })
+    .catch((err) => console.log(err));
+}
 
 // If record button is never clicked
 //upload linked to the submit button of the form
 upload = document.getElementById("submitBtn");
-upload.addEventListener("click", event => {
-  if (typeof rec == 'undefined'){ // There is no recording
+upload.addEventListener("click", (event) => {
+  if (typeof rec == "undefined") {
+    // There is no recording
     let myBody = {
       title: document.querySelector("#nameYourDream").value,
       description: document.querySelector("#description").value,
       date: document.querySelector("#date").value,
-      categories: ["Action"]
-    }
-    fetch('/recordNoVoice', {
+      categories: ["Action"],
+    };
+    fetch("/recordNoVoice", {
       method: "POST",
       body: JSON.stringify(myBody),
       headers: {
-        'Content-type': 'application/json; charset=UTF-8'
-      }
+        "Content-type": "application/json; charset=UTF-8",
+      },
     })
-    .then(() => {
-      console.log("no recording fetch done")
-      window.location.href="/dreams"
-    })
-    .catch((err) => console.log(err))
-  } else { // There is a recording, we do all the xmlhttp and fetch logic
-    //create the wav blob and pass it on to createDownloadLink 
-  rec.exportWAV(createDownloadLink);
-  }    
-	})
+      .then(() => {
+        console.log("no recording fetch done");
+        window.location.href = "/dreams";
+      })
+      .catch((err) => console.log(err));
+  } else {
+    // There is a recording, we do all the xmlhttp and fetch logic
+    rec.exportWAV(sendData);
+  }
+});
