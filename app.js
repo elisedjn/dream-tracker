3bc31c9ed7eb54c264bf69c6f69592c5a96c3c0a
Elise Djn <elisedjn@protonmail.com>
2022-12-05 09:29:25 +0100

    fix before deploy on vercel

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 require('dotenv').config();
 
-
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
 
 // require database configuration
 require('./configs/db.config');
@@ -26,12 +25,13 @@ app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(
+  require('node-sass-middleware')({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    sourceMap: true,
+  })
+);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -39,44 +39,48 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // Partials
-hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Handlebars Date Setting
-const moment = require('moment'); 
+const moment = require('moment');
 var DateFormats = {
-  short: "DD MMM",
-  long: "dddd DD.MM.YYYY"
+  short: 'DD MMM',
+  long: 'dddd DD.MM.YYYY',
 };
-hbs.registerHelper("formatDate", function(datetime, format) {
-    if (moment) {
-      format = DateFormats[format] || format;
-      return moment(datetime).format(format);
-    }
-    else {
-      return datetime;
-    }
-  });
-
+hbs.registerHelper('formatDate', function (datetime, format) {
+  if (moment) {
+    format = DateFormats[format] || format;
+    return moment(datetime).format(format);
+  } else {
+    return datetime;
+  }
+});
 
 // cookies and sessions
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
- 
-app.use(session({
+const MongoStore = require('connect-mongo');
+
+app.use(
+  session({
     secret: 'myDream',
     name: 'dreamCookie',
     cookie: {
-        maxAge: 60*60*24*1000 
+      maxAge: 60 * 60 * 24 * 1000,
     },
-    store: new MongoStore({
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
       mongooseConnection: mongoose.connection,
-      ttl: 60*60*24 
-    })
-}));
+      ttl: 60 * 60 * 24,
+    }),
+  })
+);
 
 // Clear the session
-app.use(function(req, res, next) {
-  res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+app.use(function (req, res, next) {
+  res.set(
+    'Cache-Control',
+    'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0'
+  );
   next();
 });
 
@@ -91,12 +95,10 @@ app.use('/', authRouter);
 
 // make the private routes private
 app.use((req, res, next) => {
-  req.session.loggedInUser ? next() : res.render('auth/login.hbs', {redirect: true});
+  req.session.loggedInUser ? next() : res.render('auth/login.hbs', { redirect: true });
 });
 
 const privateRouter = require('./routes/private.routes');
 app.use('/', privateRouter);
-
-
 
 module.exports = app;
--- configs/db.config.js
+++ configs/db.config.js
@@ -3,9 +3,11 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/dream-tracke
 
 mongoose
   .connect(MONGODB_URI, {
-    useCreateIndex: true,
+    // useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error('Error connecting to mongo', err));
